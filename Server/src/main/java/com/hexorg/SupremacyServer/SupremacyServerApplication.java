212559c19b6a06204e5f54fa08982e9279ea8a44
Stanislav Ponomarev <devhexorg@gmail.com>
2018-01-21 14:28:24 -0500

    Added Maven build script

---
+++ Server/src/main/java/com/hexorg/SupremacyServer/SupremacyServerApplication.java
@@ -0,0 +1,12 @@
+package com.hexorg.SupremacyServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupremacyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupremacyServerApplication.class, args);
+	}
+}
